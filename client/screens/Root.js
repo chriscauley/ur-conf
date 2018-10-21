f520adc19122c307fa009440a93cfc4813adae72
chriscauley <chris@lablackey.com>
2018-10-21 19:28:16 +0000

    separating out prepData into separate file

--- client/screens/Root.js
+++ client/screens/Root.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from './Nav';
 import TalkList from "./TalkList";
 import { loadData } from "../utils/ajax";
+import prepData from "../utils/prepData";
 
 export default class Root extends React.PureComponent {
   state = {
@@ -10,30 +11,7 @@ export default class Root extends React.PureComponent {
   }
   componentDidMount() {
     loadData().then(json => {
-      const data = json.data;
-
-      data.author_map = {};
-      data.authors.forEach(author => {
-        data.author_map[author.id] = author;
-      });
-
-      data.room_map = {};
-      data.rooms.forEach(room => {
-        data.room_map[room.id] = room;
-      })
-
-      data.talk_map = {};
-      data.talks.forEach(talk => {
-        talk.authors = talk.authors.map(a => data.author_map[a.id]);
-        console.log(data.room_map,talk.roomId);
-        talk.room = data.room_map[talk.roomId];
-        data.talk_map[talk.id] = talk
-      });
-
-      data.talkvotes.forEach(vote => {
-        data.talk_map[vote.talkId].vote = vote.vote
-      })
-      this.setState(data);
+      this.setState(prepData(json.data));
     })
     window.ROOT = this;
   }
