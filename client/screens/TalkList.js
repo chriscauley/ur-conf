9dca8320e0544107236590eb12468261b3aceac8
chriscauley <chris@lablackey.com>
2018-10-21 00:40:19 +0000

    adding basic vote buttons to talk

--- client/screens/TalkList.js
+++ client/screens/TalkList.js
@@ -1,5 +1,18 @@
 import React from "react";
 export default class TalkList extends React.Component {
+  vote(v) {
+    return () => {
+      console.log(v)
+    }
+  }
+  getVotes(talk) {
+    const votes = [
+      { verbose: "no", value: -1, icon: "em em--1" },
+      { verbose: "maybe", value: 0, icon: "em em-thinking_face" },
+      { verbose: "yes", value: 1, icon: "em em---1" },
+    ]
+    return votes
+  }
   render() {
     return (
 <div className="row">
@@ -9,6 +22,13 @@ export default class TalkList extends React.Component {
       <div className="card-content">
         <div className="card-title">{ talk.title }</div>
         <p>with { talk.authors[0].name }</p>
+      </div>
+      <div className="card-action">
+        { this.getVotes(talk).map( v=> (
+          <a onClick={this.vote(v.value)}>
+            <span className={v.icon}></span> {v.verbose}
+          </a>
+        ) ) }
       </div>
     </div>
   </div>
