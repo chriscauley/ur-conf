9a7758fa96eab8c3575e7437cb10d0c964c44cad
chriscauley <chris@lablackey.com>
2018-10-21 21:10:31 +0000

    adding date-fns and linting

--- client/screens/Schedule.js
+++ client/screens/Schedule.js
@@ -7,16 +7,16 @@ export default class Schedule extends React.Component {
     if (!timeslots) { return <div>Loading</div> }
     return (
 <div className="row">
-  <div className="col s12 m6">
+  <div className="col s12 m8 l6">
     { timeslots.map(timeslot => (
     <div className="card" key={ timeslot.id }>
       <div className="card-content">
         <div className="card-title">
           { timeslot.time_display }
         </div>
-        <ul class="collection">
+        <ul className="collection">
           { sortBy(timeslot.talk_list,['vote','title']).map(talk => (
-          <li class="collection-item" key={ talk.id }>
+          <li className="collection-item" key={ talk.id }>
             { talk.title }{ talk.vote }
           </li>
           ) ) }
