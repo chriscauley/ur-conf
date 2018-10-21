5073b107ac7c6952d1f4cd79bf42bcd27b77b3c4
chriscauley <chris@lablackey.com>
2018-10-21 19:31:26 +0000

    timeslots into the schema file.

--- main/schema.py
+++ main/schema.py
@@ -21,6 +21,7 @@ class AuthorType(DjangoObjectType):
 
 class TalkType(DjangoObjectType):
     room_id = graphene.Int(source='room_id')
+    timeslot_id = graphene.Int(source='timeslot_id')
     class Meta:
         model = Talk
 
@@ -39,7 +40,7 @@ class TalkAttendanceType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     rooms = graphene.List(RoomType)
-    timeslot = graphene.Field(TimeSlotType)
+    timeslots = graphene.List(TimeSlotType)
     authors = graphene.List(AuthorType)
     talks = graphene.List(TalkType)
     talkvotes = graphene.List(TalkVoteType)
@@ -50,6 +51,9 @@ class Query(graphene.ObjectType):
 
     def resolve_rooms(self,info):
         return Room.objects.all()
+
+    def resolve_timeslots(self,info):
+        return TimeSlot.objects.all()
 
     def resolve_talkvotes(self,info):
         return TalkVote.objects.filter(user=info.context.user)
