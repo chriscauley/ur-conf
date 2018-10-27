979a9b85efb9d7c7b62ccb38187323fe0f46a447
chriscauley <chris@lablackey.com>
2018-10-27 19:46:38 +0000

    responsive one column

--- client/screens/SignUp/index.js
+++ client/screens/SignUp/index.js
@@ -12,25 +12,23 @@ class SignUp extends React.Component {
   render() {
     const { auth } = this.props
     return (
-<div className="row">
-  <div className="col m6 offset-m3 s12">
-    <h2 className="red-text lighten-2">Sign Up</h2>
-    <Form
-       onSubmit={this.createAccount}
-       {...formSchema}>
-      <p className="center"><button className="btn btn-blue">
-          { auth.user?
-          _`Set Email`:
-          _`Create Account`
-          }
-      </button></p>
-    </Form>
-    <p className="center">
-      <button className="btn btn-blue" onclick={this.createAccount}>
-        {_`Continue as Guest`}
-      </button>
-    </p>
-  </div>
+<div className="w300">
+  <h2 className="red-text lighten-2">Sign Up</h2>
+  <Form
+     onSubmit={this.createAccount}
+     {...formSchema}>
+    <p className="center"><button className="btn btn-blue">
+        { auth.user?
+        _`Set Email`:
+        _`Create Account`
+        }
+    </button></p>
+  </Form>
+  <p className="center">
+    <button className="btn btn-blue" onclick={this.createAccount}>
+      {_`Continue as Guest`}
+    </button>
+  </p>
 </div>
 )
   }
