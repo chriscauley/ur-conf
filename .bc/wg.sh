b6a5d8936c059eea61061bc44e0101018cffaf00
chriscauley <chris@lablackey.com>
2018-10-29 23:32:46 +0000

    starting barcamp downlowd script

---
+++ .bc/wg.sh
@@ -0,0 +1,6 @@
+wget http://s.barcampphilly.org/events/18/event_dates/17 -O bc2011.html
+wget http://s.barcampphilly.org/events/19/event_dates/18 -O bc2013.html
+wget http://s.barcampphilly.org/events/20/event_dates/19 -O bc2014.html
+wget http://s.barcampphilly.org/events/21/event_dates/20 -O bc2015.html
+wget http://s.barcampphilly.org/events/22/event_dates/21 -O bc2016.html
+wget http://s.barcampphilly.org/events/23/event_dates/22 -O bc2017.html
