e818e91408ce79ce16437c34f03c72b7d2df7196
chriscauley <chris@lablackey.com>
2018-11-03 00:26:59 +0000

    shifting default date to 930

--- client/lib/date.js
+++ client/lib/date.js
@@ -7,8 +7,8 @@ const date = {
     date.value = date.now().valueOf() + date.SPEED * 1000
     date.visible && date.visible.forceUpdate()
   },
-  value: new Date('2017-10-14 8:00'.valueOf()),
-  reset: () => (date.value = new Date('2017-10-14 8:00'.valueOf())),
+  value: new Date('2017-10-14 9:30'.valueOf()),
+  reset: () => (date.value = new Date('2017-10-14 9:30'.valueOf())),
   print: () => format(date.now(), 'h:mm'),
   isNow: timeslot => {
     const now = date.now().valueOf()
