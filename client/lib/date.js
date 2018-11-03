1b93b1c0095b020fbfb8f6828807308caf9dc492
chriscauley <chris@lablackey.com>
2018-11-03 21:47:40 +0000

    reset date at end

--- client/lib/date.js
+++ client/lib/date.js
@@ -2,13 +2,17 @@ import { format } from 'date-fns'
 
 const date = {
   SPEED: 0, // seconds per tick
+  start: new Date('2017-10-14 9:30'.valueOf()),
+  end: new Date('2017-10-14 17:30'.valueOf()),
   now: () => new Date(date.value),
   tick: () => {
     date.value = date.now().valueOf() + date.SPEED * 60 * 1000
+    if (date.value > date.end) {
+      date.value = date.start
+    }
     date.visible && date.visible.forceUpdate()
   },
-  value: new Date('2017-10-14 9:30').valueOf(),
-  reset: () => (date.value = new Date('2017-10-14 9:30'.valueOf())),
+  reset: () => (date.value = date.start),
   print: () => format(date.now(), 'h:mm'),
   isNow: timeslot => {
     return timeslot.DATE < date.value && timeslot.END_DATE > date.value
@@ -18,4 +22,5 @@ const date = {
   },
 }
 
+date.reset()
 export default date
