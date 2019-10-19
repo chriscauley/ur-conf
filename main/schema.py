de8cd3fa7fd7f7b0444cd6993e625ac5a48d7652
chriscauley <chris@lablackey.com>
2019-10-19 03:11:34 +0000

    hardcoding conference id for now and removing unused resolve functions

--- main/schema.py
+++ main/schema.py
@@ -24,8 +24,8 @@ from main.types import (
     LocationType,
 )
 
-
-_id = 1
+#! TODO un-hard-code
+_id = 3
 
 
 class Query(graphene.ObjectType):
@@ -45,19 +45,10 @@ class Query(graphene.ObjectType):
         return info.context.user
 
     def resolve_conference(self, info, id=None):
-        return Conference.objects.get(id=id)
+        return Conference.objects.get(id=_id)
 
     def resolve_conferences(self, info):
         return Conference.objects.all()
-
-    def resolve_talks(self, info):
-        return Talk.objects.filter(conference_id=_id).prefetch_related("authors")
-
-    def resolve_rooms(self, info):
-        return Room.objects.filter(conference_id=_id)
-
-    def resolve_timeslots(self, info):
-        return TimeSlot.objects.filter(conference_id=_id)
 
     def resolve_talkvotes(self, info):
         if not info.context.user.is_authenticated:
@@ -68,9 +59,6 @@ class Query(graphene.ObjectType):
         if not info.context.user.is_authenticated:
             return []
         return TalkAttendance.objects.filter(user=info.context.user)
-
-    def resolve_authors(self, info):
-        return Author.objects.filter(conference_id=_id)
 
 
 schema = graphene.Schema(query=Query)
