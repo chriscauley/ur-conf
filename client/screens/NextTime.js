6014dc4c1b5a5157e484624266600277dffa3ca7
chriscauley <chris@lablackey.com>
2019-10-19 16:29:47 +0000

    redirect to next active timeslot (instead of past time)

--- client/screens/NextTime.js
+++ client/screens/NextTime.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { isBefore, addMinutes } from 'date-fns'
 
 import navigate from '../lib/navigate'
 import { withTalks } from '../graphql'
@@ -8,10 +9,19 @@ class NextTime extends React.Component {
     if (this.props.talkGQL.loading) {
       return null
     }
-    const slot = this.props.talkGQL.conference.timeslotSet.find(ts => {
-      return ts.sortableTalks.length > 0
+
+    const talkSlots = this.props.talkGQL.conference.timeslotSet.filter(
+      ts => ts.sortableTalks.length > 0
+    )
+    const nextSlot = talkSlots.find(ts => {
+      const date = addMinutes(ts.datetime, 30)
+      return isBefore(new Date(), date)
     })
-    navigate(`${this.props.path}${slot.id}/`)
+    if (nextSlot) {
+      navigate(`${this.props.path}${nextSlot.id}/`)
+    } else {
+      navigate(`${this.props.path}${talkSlots[0].id}/`)
+    }
     return null
   }
 }
